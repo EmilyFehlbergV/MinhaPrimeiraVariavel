467ca09e4ab48496c0a362f0a839f801faad9c3d
Emily Fehlberg Visintin <fehlviimusics@gmail.com>
2026-09-16 16:55:56 -0300

    cria variavel para usar depois

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// Este programa deverá salvar o nome do usuário dentro de uma variável
+// Este programa deverá exibir uma mensagem de saudação mostrando o nome do usuário
+var nomeUsuario  = "Fehlvii";
+Console.Write("Boas vindas, ");
+Console.WriteLine(nomeUsuario);
